f364c613a1d1028d9f6a35dbbae8f20396b9b647
LikerK <nagorniykirill@gmail.com>
2024-03-29 09:47:06 +0300

    add list students

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -1,29 +1,42 @@
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { showModal } from '../slices/modals';
+import { ReactComponent as ChildIcon } from '../assets/child-icon.svg';
 import ModalStudent from './Modal.jsx'
 import { selectors } from '../slices/students';
 
 const Students = () => {
   const dispatch = useDispatch();
   const students = useSelector(selectors.selectAll);
+  console.log(students);
   const setShowModal = (type, item = null) => dispatch(showModal({ type, item }));
   return (
-    <div className="container">
-      {students.map((student) => {
-        return (
-          <div key={student.id} className="border rounded p-2 my-2">
-            <span>{student.name}</span>
-          </div>
-        )
-      })}
+    <div className="d-flex flex-column justify-content-between h-100 my-3 rounded bg-light">
+      <div>
+        {students.map((student) => {
+          return (
+            <div key={student.id} className="d-flex justify-content-between border rounded p-2 m-2">
+              <div>
+                <ChildIcon className="m-1" />
+                <span className='text-bold'>{student.name}</span>
+              </div>
+              <Button
+                variant='light'
+                className='border'
+              >
+                Open
+              </Button>
+            </div>
+          )
+        })}
+      </div>
       <Button
-        variant="primary"
+        variant="light"
         type="button"
-        className="mt-auto"
+        className="m-2 border border-2"
         onClick={() => setShowModal('adding')}
       >
-        <span>Добавить ученика</span>
+        <span>Добавить группу</span>
       </Button>
       <ModalStudent />
     </div>
