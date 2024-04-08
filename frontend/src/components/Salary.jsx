e73446155d0b0388e37a8f130e0c58a87802de12
LikerK <nagorniykirill@gmail.com>
2024-04-08 10:20:50 +0300

    add lessons per month

--- frontend/src/components/Salary.jsx
+++ frontend/src/components/Salary.jsx
@@ -1,3 +1,22 @@
-const Salary = () => <h1>Зарплата</h1>
+import { useSelector } from 'react-redux';
+import { selectors } from '../slices/lessons';
+
+const Salary = () => {
+    const lessons = useSelector(selectors.selectAll);
+    const date = new Date()
+    const monthName = date.toLocaleString('default', { month: 'long' });
+    const lessonsMonth = lessons.filter((lesson) => {
+        const lessonDate = new Date(lesson.lessonDate)
+        const lessonMonth = lessonDate.toLocaleString('default', { month: 'long' });
+        return lessonMonth === monthName && date.getDate() >= lessonDate.getDay();
+    });
+    
+    // const getPrice = (type) => {
+    //   if (type)
+    // };
+    // const salary = lessonsMonth.reduce((acc, lesson) => acc + number, 0)
+    return <span></span>
+
+}
 
 export default Salary;
